f39ae072202d9afc5209ebf16381203455373f38
Maria del Mar <maria.cegarra2303@gmail.com>
2024-03-16 18:04:07 +0100

    Refactor test to match with refactor of function

--- src/controllers/credit-card-validator/creditCardValidator.spec.ts
+++ src/controllers/credit-card-validator/creditCardValidator.spec.ts
@@ -3,7 +3,11 @@ import { creditCardValidator } from './creditCardValidator';
 describe('Given the creditCardValidator function', () => {
 	describe('When it receives 2 strings as parameters', () => {
 		test('Then it should return a boolean', () => {
-			expect(creditCardValidator('string', 'string')).toBe(true);
+			const CreditCardInfo = {
+				creditCardNumber: '4583 3953',
+				expiryDate: '23/23',
+			};
+			expect(creditCardValidator(CreditCardInfo)).toBe(true);
 		});
 	});
 });
