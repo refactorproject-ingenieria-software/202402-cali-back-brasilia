b587fdb8d5640b5ea204c24fe2c284c05b92f9ba
Maria del Mar <maria.cegarra2303@gmail.com>
2024-03-16 17:44:02 +0100

    Fist test CreditCardValidator

---
+++ src/controllers/credit-card-validator/credit-card-validator.spec.ts
@@ -0,0 +1,7 @@
+describe('Given the creditCardValidator function', () => {
+	describe('When it receives 2 strings as parameters', () => {
+		test('Then it should return a boolean', () => {
+			expect(creditCardValidator('string', 'string')).toBe(true);
+		});
+	});
+});
