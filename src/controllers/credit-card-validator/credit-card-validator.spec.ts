8663700c070ac4502ecf0460e24152c425a09d53
Maria del Mar <maria.cegarra2303@gmail.com>
2024-03-16 17:47:30 +0100

    Minimum functionality to pass test

--- src/controllers/credit-card-validator/credit-card-validator.spec.ts
+++ src/controllers/credit-card-validator/credit-card-validator.spec.ts
@@ -1,3 +1,9 @@
+const creditCardValidator = (a, b) => {
+	if (a && b) {
+		return true;
+	}
+};
+
 describe('Given the creditCardValidator function', () => {
 	describe('When it receives 2 strings as parameters', () => {
 		test('Then it should return a boolean', () => {
