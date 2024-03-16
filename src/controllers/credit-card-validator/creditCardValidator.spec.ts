92f28d6ff681116975af899b878c7ab54b6c77b6
Maria del Mar <maria.cegarra2303@gmail.com>
2024-03-16 18:16:09 +0100

    Write test to credit card number validation

--- src/controllers/credit-card-validator/creditCardValidator.spec.ts
+++ src/controllers/credit-card-validator/creditCardValidator.spec.ts
@@ -1,4 +1,7 @@
-import { creditCardValidator } from './creditCardValidator';
+import {
+	CreaditCardValidationResult,
+	creditCardValidator,
+} from './creditCardValidator';
 
 describe('Given the creditCardValidator function', () => {
 	describe('When it receives 2 strings as parameters', () => {
@@ -8,6 +11,23 @@ describe('Given the creditCardValidator function', () => {
 				expiryDate: '23/23',
 			};
 			expect(creditCardValidator(CreditCardInfo)).toBe(true);
+		});
+	});
+
+	describe('When it receives a card number with less than 16 digits', () => {
+		test("Then it should return false and an error message: 'The card must have at least 16 digits'", () => {
+			const CreditCardInfo = {
+				creditCardNumber: '4583 3953',
+				expiryDate: '23/23',
+			};
+			const expectedCreaditCardValidationResult: CreaditCardValidationResult = {
+				isValid: false,
+				errorMessage: 'The card must have at least 16 digits',
+			};
+
+			expect(creditCardValidator(CreditCardInfo)).toStrictEqual(
+				expectedCreaditCardValidationResult,
+			);
 		});
 	});
 });
