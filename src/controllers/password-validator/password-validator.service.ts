807d7be9f5e5bfe4ed89a6b5964a7f3ad7e146fd
Joacco <joaquingodoy2407@gmail.com>
2024-03-18 20:11:50 +0100

    feat(password-validator-controller): validate that the password has at least 2 numbers

--- src/controllers/password-validator/password-validator.service.ts
+++ src/controllers/password-validator/password-validator.service.ts
@@ -6,11 +6,15 @@ export class PasswordValidatorService {
 	validatePassword(password: string): PasswordValidatorResponse {
 		const errors: string[] = [];
 		const isValidLength = password.length >= 8;
+		const containsTwoNumbers = password.match(/[0-9]/g).length >= 2;
 
 		if (!isValidLength) errors.push('Password must be at least 8 characters');
 
+		if (!containsTwoNumbers)
+			errors.push('Password must contain at least 2 numbers');
+
 		return {
-			isValid: isValidLength,
+			isValid: isValidLength && containsTwoNumbers,
 			errors: errors,
 		};
 	}
