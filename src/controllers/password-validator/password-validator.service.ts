3ac87c5b3f857ce6c24aeb3e3e9fd3b93f5a0192
Joacco <joaquingodoy2407@gmail.com>
2024-03-18 20:00:40 +0100

    feat(password-validator-controller): validate password length

--- src/controllers/password-validator/password-validator.service.ts
+++ src/controllers/password-validator/password-validator.service.ts
@@ -4,8 +4,14 @@ import { PasswordValidatorResponse } from './password-validator.types';
 @Injectable()
 export class PasswordValidatorService {
 	validatePassword(password: string): PasswordValidatorResponse {
-		// TODO: Validation logic.
-		const asd = password;
-		return asd as unknown as PasswordValidatorResponse;
+		const errors: string[] = [];
+		const isValidLength = password.length >= 8;
+
+		if (!isValidLength) errors.push('Password must be at least 8 characters');
+
+		return {
+			isValid: isValidLength,
+			errors: errors,
+		};
 	}
 }
